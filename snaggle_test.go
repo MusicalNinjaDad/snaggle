e9b0968f0d9407d8a71bff7c404de50e772553f3
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-14 10:58:14 +0000

    use testify

--- snaggle_test.go
+++ snaggle_test.go
@@ -3,14 +3,15 @@ package snaggle
 import (
 	"slices"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLddSingleBinary(t *testing.T) {
+	Assert := assert.New(t)
 	expectedLibs := []string{"/lib64/libc.so.6", "/lib64/ld-linux-x86-64.so.2"}
 	slices.Sort(expectedLibs)
 	libs, err := LibPaths("/usr/sbin/which")
+	Assert.NoError(err)
 	slices.Sort(libs)
-	if !slices.Equal(libs, expectedLibs) || err != nil {
-		t.Error("Expected", expectedLibs, "got", libs, "with error", err)
-	}
+	Assert.Equal(libs, expectedLibs)
 }
