e0f31ce477937cfe954bc90f679f1935db61a7c7
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-14 11:45:13 +0000

    start to identify  a symlink tree

--- snaggle.go
+++ snaggle.go
@@ -2,6 +2,8 @@ package snaggle
 
 import (
 	"github.com/u-root/u-root/pkg/ldd"
+	"os"
+	"path/filepath"
 	"slices"
 )
 
@@ -9,4 +11,15 @@ func LibPaths(bin string) ([]string, error) {
 	libs, err := ldd.List(bin)
 	slices.Sort(libs)
 	return libs, err
+}
+
+func SymlinkTree(path string) (map[string]string, error) {
+	path, err := filepath.Abs(path)
+	if err != nil {
+		return nil, err
+	}
+	tree := make(map[string]string)
+	leaf, err := os.Readlink(path)
+	tree[path] = leaf
+	return tree, err
 }
--- snaggle_test.go
+++ snaggle_test.go
@@ -2,6 +2,7 @@ package snaggle
 
 import (
 	"github.com/stretchr/testify/assert"
+	"path/filepath"
 	"slices"
 	"testing"
 )
@@ -22,4 +23,14 @@ func TestSymlinkedBinary(t *testing.T) {
 	libs, err := LibPaths("test/assets/which")
 	Assert.NoError(err)
 	Assert.Equal(libs, expectedLibs)
+}
+
+func TestSymlinkTree(t *testing.T) {
+	Assert := assert.New(t)
+	expectedTree := make(map[string]string)
+	link, _ := filepath.Abs("test/assets/which")
+	expectedTree[link] = "/usr/sbin/which"
+	tree, err := SymlinkTree("test/assets/which")
+	Assert.NoError(err)
+	Assert.Equal(tree, expectedTree)
 }
