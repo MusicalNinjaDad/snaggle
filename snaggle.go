4f0ce48671ede615dbcbc7d5e87c0e4fe3d1bb19
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-14 17:22:38 +0000

    CopyBinary to newRoot

--- snaggle.go
+++ snaggle.go
@@ -1,12 +1,30 @@
 package snaggle
 
 import (
-	"github.com/u-root/u-root/pkg/ldd"
+	"os"
+	"path/filepath"
 	"slices"
+
+	"github.com/u-root/u-root/pkg/ldd"
 )
 
 func LibPaths(bin string) ([]string, error) {
 	libs, err := ldd.List(bin)
 	slices.Sort(libs)
 	return libs, err
+}
+
+func CopyBinary(bin string, newRoot string) (string, error) {
+	bin, err := filepath.Abs(bin)
+	target := filepath.Join(newRoot, bin)
+	if err != nil {
+		return target, err
+	}
+
+	os.MkdirAll(filepath.Dir(target), os.ModePerm)
+
+	// TODO: handle err (e.g. "operation not permitted")
+	// TODO: what if bin is a symlink?
+	err = os.Link(bin, target)
+	return target, err
 }
--- snaggle_test.go
+++ snaggle_test.go
@@ -1,6 +1,9 @@
 package snaggle
 
 import (
+	"errors"
+	"os"
+	"path/filepath"
 	"slices"
 	"testing"
 
@@ -23,4 +26,50 @@ func TestSymlinkedBinary(t *testing.T) {
 	libs, err := LibPaths("symlink/testdata/which")
 	Assert.NoError(err)
 	Assert.Equal(expectedLibs, libs)
+}
+
+func pwd(t *testing.T) string {
+	t.Helper()
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal("Failed to get pwd. Error:", err)
+	}
+	return pwd
+}
+
+func workspaceTempDir(t *testing.T) string {
+	t.Helper()
+	tmpRoot := filepath.Join(pwd(t), ".tmp")
+	err := os.Mkdir(tmpRoot, os.ModePerm)
+	if err != nil && !errors.Is(err, os.ErrExist) {
+		t.Fatal("Failed to create ./.tmp. Error: ", err)
+	}
+	tmp, err := os.MkdirTemp(tmpRoot, t.Name())
+	if err != nil {
+		t.Fatal("Failed to create", tmp, "Error:", err)
+	}
+	t.Cleanup(func() { os.RemoveAll(tmp) })
+	return tmp
+}
+
+func SameFile(t *testing.T, path1 string, path2 string) bool {
+	t.Helper()
+	file1, err1 := os.Stat(path1)
+	file2, err2 := os.Stat(path2)
+	if err1 != nil || err2 != nil {
+		t.Log("Errors stating:", path1, "-", err1, ";", path2, "-", err2)
+		return false
+	}
+	return os.SameFile(file1, file2)
+}
+
+func TestCopyBinary(t *testing.T) {
+	Assert := assert.New(t)
+	tmp := workspaceTempDir(t)
+	expectedFile := filepath.Join(tmp, pwd(t), "/testdata/which")
+	path, err := CopyBinary("testdata/which", tmp)
+	Assert.NoError(err)
+	Assert.Equal(expectedFile, path)
+	Assert.FileExists(expectedFile)
+	Assert.True(SameFile(t, expectedFile, path))
 }
