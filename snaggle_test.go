b747221ea6d1435a5031ea439f6be476e8631341
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-14 11:22:12 +0000

    check handling symlinked binaries

--- snaggle_test.go
+++ snaggle_test.go
@@ -14,3 +14,12 @@ func TestLddSingleBinary(t *testing.T) {
 	Assert.NoError(err)
 	Assert.Equal(libs, expectedLibs)
 }
+
+func TestSymlinkedBinary(t *testing.T) {
+	Assert := assert.New(t)
+	expectedLibs := []string{"/lib64/libc.so.6", "/lib64/ld-linux-x86-64.so.2"}
+	slices.Sort(expectedLibs)
+	libs, err := LibPaths("test/assets/which")
+	Assert.NoError(err)
+	Assert.Equal(libs, expectedLibs)
+}
