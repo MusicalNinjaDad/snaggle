87f1d68484bfd78803fd90bd30fe983b0d83e714
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-15 06:29:08 +0000

    missed one ugly idiomatic early return on err

--- snaggle.go
+++ snaggle.go
@@ -20,6 +20,12 @@ func LibPaths(bin string) ([]string, error) {
 // E.g. `LinkFile(/usr/bin/which, /tmp)` will create a link at `/tmp/usr/bin/which`.
 //
 // Note: the _absolute_ `path` will be used, even if a relative path is provided.
+// 
+// Errors may be propogated from `filepath.Join`, `os.MkdirAll` and `os.Link`, sadly a complete
+// lack of meaningful documentation on stdlib errors means the author of this code can't give
+// any guidance on what they may be.
+// PRs are always welcome to improve error handling or documentation.
+//
 func LinkFile(path string, newRoot string) (string, error) {
 	path, err := filepath.Abs(path)
 	target := filepath.Join(newRoot, path)
@@ -27,7 +33,10 @@ func LinkFile(path string, newRoot string) (string, error) {
 		return target, err
 	}
 
-	os.MkdirAll(filepath.Dir(target), os.ModePerm)
+	err = os.MkdirAll(filepath.Dir(target), os.ModePerm)
+	if err != nil {
+		return target, err
+	}
 
 	// TODO: handle err (e.g. "operation not permitted")
 	// TODO: what if bin is a symlink?
