7835b1d5506405e7752e26bfdaded2e743b9fc75
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-19 10:26:21 +0000

    fix variable naming in Testldd_static

--- elf/elf_unit_test.go
+++ elf/elf_unit_test.go
@@ -37,8 +37,8 @@ func TestLdd_nested(t *testing.T) {
 
 func TestLdd_static(t *testing.T) {
 	Assert := assert.New(t)
-	id := filepath.Join(pwd(t), "../testdata/hello_static")
-	dependencies, err := ldd(id)
+	static := filepath.Join(pwd(t), "../testdata/hello_static")
+	dependencies, err := ldd(static)
 	Assert.ErrorIs(err, ErrElf)
 	Assert.ErrorIs(err, ErrElfLdd)
 	Assert.Nil(dependencies)
