a4f63bf4c168fff9bd1c9db012b4f94c1f5856d2
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-20 11:19:54 +0000

    Table test FileErrors

--- elf/elf_errors_test.go
+++ elf/elf_errors_test.go
@@ -13,20 +13,61 @@ import (
 	. "github.com/MusicalNinjaDad/snaggle/internal"
 )
 
-func TestFileNotFound(t *testing.T) {
-	Assert := assert.New(t)
-	const path = "/bad/path"
-	bad, err := New(path)
-	var errelf *ErrElf
-	Assert.Equal("path", bad.Name)
-	Assert.Equal(path, bad.Path)
-	Assert.ErrorIs(err, fs.ErrNotExist)
-	Assert.NotErrorIs(err, ErrInvalidElf)
-	Assert.ErrorAs(err, &errelf)
-	errpath := err.(*ErrElf).Path()
-	Assert.Equal(path, errpath)
-	Assert.ErrorContains(err, "error(s) parsing /bad/path:")
-	Assert.ErrorContains(err, "no such file or directory")
+func readOnlyFile(t *testing.T) *os.File {
+	t.Helper()
+	tmp := t.TempDir()
+	path := filepath.Join(tmp, "noaccess")
+	noaccess, err := os.Create(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	noaccess.Chmod(0222) // --w--w--w-
+	return noaccess
+}
+
+func TestFileErrors(t *testing.T) {
+	tests := []struct {
+		name      string
+		filename  string
+		path      string
+		err       error
+		errorText string
+	}{
+		{
+			name:      "DirectoryNotFound",
+			filename:  "nonexistant",
+			path:      filepath.Join(Pwd(t), "../testdata/nothere/nonexistant"),
+			err:       fs.ErrNotExist,
+			errorText: "no such file or directory",
+		},
+		{
+			name:      "Access Denied",
+			filename:  "noaccess",
+			path:      readOnlyFile(t).Name(),
+			err:       fs.ErrPermission,
+			errorText: "permission denied",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			Assert := assert.New(t)
+			var errelf *ErrElf
+			parsed, err := New(tc.path)
+			// We should have basic info in ALL cases
+			Assert.Equal(parsed.Name, tc.filename)
+			Assert.Equal(parsed.Path, tc.path)
+
+			Assert.ErrorIs(err, tc.err)
+			Assert.ErrorContains(err, tc.errorText)
+
+			Assert.ErrorAs(err, &errelf)
+			Assert.Equal(tc.path, errelf.Path())
+			Assert.ErrorContains(err, "error(s) parsing "+tc.path+":")
+
+			Assert.NotErrorIs(err, ErrInvalidElf)
+		})
+	}
 }
 
 func TestNotElf(t *testing.T) {
@@ -65,27 +106,4 @@ func TestLdd_unsupported_interpreter(t *testing.T) {
 	Assert.ErrorIs(err, errors.ErrUnsupported)
 	Assert.ErrorContains(err, "invalid ELF file: unsupported operation (unsupported interpreter) '"+interpreter+"'")
 	Assert.Nil(dependencies)
-}
-
-func TestIO_Error(t *testing.T) {
-	Assert := assert.New(t)
-	tmp := t.TempDir()
-	path := filepath.Join(tmp, "noaccess")
-	noaccess, err := os.Create(path)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer os.Remove(noaccess.Name())
-	noaccess.Chmod(0222) // --w--w--w-
-	bad, err := New(noaccess.Name())
-	var errelf *ErrElf
-	Assert.Equal("noaccess", bad.Name)
-	Assert.Equal(path, bad.Path)
-	Assert.ErrorIs(err, fs.ErrPermission)
-	Assert.NotErrorIs(err, ErrInvalidElf)
-	Assert.ErrorAs(err, &errelf)
-	errpath := err.(*ErrElf).Path()
-	Assert.Equal(path, errpath)
-	Assert.ErrorContains(err, "error(s) parsing "+path+":")
-	Assert.ErrorContains(err, "permission denied")
 }
