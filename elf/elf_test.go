97bb2c0cc1c6bd18ad852eb842c69aee6c94f85e
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-21 09:51:17 +0000

    use tc for testcase

--- elf/elf_test.go
+++ elf/elf_test.go
@@ -13,16 +13,16 @@ import (
 func TestCommonBinaries(t *testing.T) {
 	tests := CommonBinaries(t)
 
-	for _, tt := range tests {
-		t.Run(tt.Description, func(t *testing.T) {
-			path := tt.ExpectedElf.Path
+	for _, tc := range tests {
+		t.Run(tc.Description, func(t *testing.T) {
+			path := tc.ExpectedElf.Path
 			Assert := assert.New(t)
 			parsed, err := elf.New(path)
 			Assert.NoError(err)
-			Assert.Equal(tt.Exe, parsed.IsExe())
-			Assert.Equal(tt.Lib, parsed.IsLib())
-			Assert.Equal(tt.Dynamic, parsed.IsDyn())
-			Assert.Nil(parsed.Diff(tt.ExpectedElf))
+			Assert.Equal(tc.Exe, parsed.IsExe())
+			Assert.Equal(tc.Lib, parsed.IsLib())
+			Assert.Equal(tc.Dynamic, parsed.IsDyn())
+			Assert.Nil(parsed.Diff(tc.ExpectedElf))
 		})
 	}
 }
