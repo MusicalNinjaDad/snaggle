2d35f1148e287cfd910cb336152c365c93ad9606
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-22 12:51:50 +0000

    remove unused linkTree

--- snaggle.go
+++ snaggle.go
@@ -20,36 +20,6 @@ func init() {
 	log.SetFlags(0)
 }
 
-// linkTree creates a hardlink to `path` under `newRoot`, preserving the full directory
-// structure similar to how `cp -r` does.
-//
-// E.g. `linkTree(/usr/bin/which, /tmp)` will create a link at `/tmp/usr/bin/which`.
-//
-// Note: the _absolute_ `path` will be used, even if a relative path is provided.
-//
-// Errors may be propogated from `filepath.Abs`, `os.MkdirAll` and `os.Link`, sadly a complete
-// lack of meaningful documentation on stdlib errors means the author of this code can't give
-// any guidance on what they may be.
-// PRs are always welcome to improve error handling or documentation.
-func linkTree(path string, newRoot string) (string, error) {
-	path, err := filepath.Abs(path)
-	target := filepath.Join(newRoot, path)
-	if err != nil {
-		// propogate err from filepath.Abs, with maybe some sort of target
-		return target, err
-	}
-
-	err = os.MkdirAll(filepath.Dir(target), os.ModePerm)
-	if err != nil {
-		return target, err
-	}
-
-	// TODO: handle err (e.g. "operation not permitted")
-	// TODO: what if bin is a symlink?
-	err = os.Link(path, target)
-	return target, err
-}
-
 // create a hardlink in targetDir which references sourcePath,
 // falls back to cp -a if sourcePath and targetDir are on different
 // filesystems.
