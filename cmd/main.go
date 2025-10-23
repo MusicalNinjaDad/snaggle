21f3dc317ab0bc2e7e80ac0e76fd042afa239f50
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-23 08:03:41 +0000

    flatten cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,17 @@
-package cmd
+package main
 
 import (
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+func main() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
 
 var rootCmd = &cobra.Command{
 	Use:   "snaggle FILE DESTINATION",
@@ -25,15 +32,6 @@ preserving filemode and attempting to preserve ownership.
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
-}
-
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
 }
 
 func init() {
--- main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/MusicalNinjaDad/snaggle/cmd"
-
-func main() {
-	cmd.Execute()
-}
