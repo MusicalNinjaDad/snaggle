bda2b4be1357cf1b81f61a3ddf59b884575fb61d
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-23 08:30:07 +0000

    test common binaries

--- snaggle/cli_test.go
+++ snaggle/cli_test.go
@@ -2,13 +2,46 @@ package main
 
 import (
 	"os/exec"
+	"path/filepath"
+	"slices"
+	"strings"
 	"testing"
 
+	"github.com/ameghdadian/x/iter"
 	"github.com/stretchr/testify/assert"
+
+	. "github.com/MusicalNinjaDad/snaggle/internal"
+	. "github.com/MusicalNinjaDad/snaggle/internal/testing"
 )
 
-func TestXxx(t *testing.T) {
-	cmd := exec.Command("./snaggle")
-	err := cmd.Run()
-	assert.NoError(t, err)
+func TestCommonBinaries(t *testing.T) {
+	tests := CommonBinaries(t)
+	for _, tc := range tests {
+		t.Run(tc.Description, func(t *testing.T) {
+			Assert := assert.New(t)
+			tmp := WorkspaceTempDir(t)
+			snaggle := exec.Command("./snaggle", tc.ExpectedElf.Path, tmp)
+
+			binPath := filepath.Join(tmp, "bin", filepath.Base(tc.ExpectedElf.Name))
+			expectedOut := make([]string, 0, 1+len(tc.ExpectedElf.Dependencies))
+			expectedOut = append(expectedOut, tc.ExpectedElf.Path+" -> "+binPath)
+			var libCopies []string
+			for _, lib := range tc.ExpectedElf.Dependencies {
+				copy := filepath.Join(tmp, "lib64", filepath.Base(lib))
+				libCopies = append(libCopies, copy)
+				expectedOut = append(expectedOut, lib+" -> "+copy)
+			}
+
+			stdout, err := snaggle.Output()
+			Assert.NoError(err)
+			AssertSameInode(t, tc.ExpectedElf.Path, binPath)
+			for idx, copy := range libCopies {
+				original := tc.ExpectedElf.Dependencies[idx]
+				same := SameFile(original, copy)
+				assert.Truef(t, same, "%s & %s are different files", original, copy)
+			}
+
+			Assert.Equal(expectedOut, slices.Collect(iter.Map((strings.Lines(string(stdout))), strings.TrimSpace)))
+		})
+	}
 }
