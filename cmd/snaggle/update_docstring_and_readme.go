ee335175f8e2656311bd401a4836690bd5e319bc
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-30 14:51:37 +0000

    check whether main.go was updated

--- cmd/snaggle/update_docstring_and_readme.go
+++ cmd/snaggle/update_docstring_and_readme.go
@@ -12,20 +12,43 @@ package main
 import (
 	"os"
 	"os/exec"
+	"slices"
 
 	. "github.com/MusicalNinjaDad/snaggle/internal"
 )
 
 func main() {
+	exitcode := 0
+	main_go, err := HashFile("main.go")
+	if err != nil {
+		println(err)
+		os.Exit(3)
+	}
+
 	snaggleBin := Build(nil)
 	helptext, err := exec.Command(snaggleBin, "--help").Output()
 	if err != nil {
 		println(err)
 		os.Exit(3)
 	}
+
 	err = SetDocComment("main.go", string(helptext))
 	if err != nil {
 		println(err)
 		os.Exit(3)
 	}
+
+	new_main, err := HashFile("main.go")
+	if err != nil {
+		println(err)
+		os.Exit(3)
+	}
+
+	if !slices.Equal(new_main, main_go) {
+		println("main.go updated")
+		exitcode = 1
+	}
+
+	os.Exit(exitcode)
+
 }
