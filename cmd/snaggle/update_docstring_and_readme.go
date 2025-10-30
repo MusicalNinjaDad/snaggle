47386c850b3ce7b4e452a1136ea2f4bda7968df8
Mike Foster <i.am.mike.foster@gmail.com>
2025-10-30 15:06:49 +0000

    safer paths & change validation

--- cmd/snaggle/update_docstring_and_readme.go
+++ cmd/snaggle/update_docstring_and_readme.go
@@ -10,48 +10,57 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 	"slices"
 
 	. "github.com/MusicalNinjaDad/snaggle/internal"
 )
 
 func main() {
+	_, thisFile, _, _ := runtime.Caller(0)
+	thisDir := filepath.Dir(thisFile)
+	// workspaceRoot := filepath.Join(thisDir, "../..")
+
 	exitcode := 0
 
-	main_go, err := HashFile("main.go")
+	main_go := filepath.Join(thisDir, "main.go")
+
+	orig_main, err := HashFile(main_go)
 	if err != nil {
-		println(err)
+		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(3)
 	}
 
 	snaggleBin := Build(nil)
 	helptext, err := exec.Command(snaggleBin, "--help").Output()
 	if err != nil {
-		println(err)
+		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(3)
 	}
 
-	err = SetDocComment("main.go", string(helptext))
+	err = SetDocComment(main_go, string(helptext))
 	if err != nil {
-		println(err)
+		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(3)
 	}
 
-	new_main, err := HashFile("main.go")
+	err = exec.Command("go", "fmt", main_go).Run()
 	if err != nil {
-		println(err)
+		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(3)
 	}
 
-	err = exec.Command("go", "fmt", "main.go").Run()
+	new_main, err := HashFile(main_go)
 	if err != nil {
-		println(err)
+		fmt.Fprintf(os.Stderr, "%s", err)
 		os.Exit(3)
 	}
 
-	if !slices.Equal(new_main, main_go) {
+	if !slices.Equal(new_main, orig_main) {
 		println("main.go updated")
 		exitcode = 1
 	}
