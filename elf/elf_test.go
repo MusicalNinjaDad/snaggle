369f867b1e423711b5e5e96ebc92e686ac2592f9
Mike Foster <i.am.mike.foster@gmail.com>
2025-11-03 12:59:38 +0000

    loop directly over GoodElfs

--- elf/elf_test.go
+++ elf/elf_test.go
@@ -30,16 +30,17 @@ func TestCommonBinaries(t *testing.T) {
 }
 
 func Test(t *testing.T) {
-	for t, tc := range TestCases(t) {
-		Assert := assert.New(t)
+	for name, details := range GoodElfs {
+		t.Run(name, func(t *testing.T) {
+			Assert := assert.New(t)
 
-		parsed, err := elf.New(tc.Src)
+			parsed, err := elf.New(details.Elf.Path)
 
-		Assert.NoError(err)
-		Assert.Equal(tc.ExpectedBinary.Exe, parsed.IsExe())
-		Assert.Equal(tc.ExpectedBinary.Lib, parsed.IsLib())
-		Assert.Equal(tc.ExpectedBinary.Dynamic, parsed.IsDyn())
-		Assert.Nil(parsed.Diff(tc.ExpectedBinary.Elf))
-
+			Assert.NoError(err)
+			Assert.Equal(details.Exe, parsed.IsExe())
+			Assert.Equal(details.Lib, parsed.IsLib())
+			Assert.Equal(details.Dynamic, parsed.IsDyn())
+			Assert.Nil(parsed.Diff(details.Elf))
+		})
 	}
 }
